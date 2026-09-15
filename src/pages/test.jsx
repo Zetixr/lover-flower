71ce48ec1340ed9243d2a2fdb96592767e3363aa
Zetixr <zetixr@mai.ru>
2026-09-15 13:50:40 +0500

    fifth commit

--- src/pages/test.jsx
+++ src/pages/test.jsx
@@ -1,45 +1,36 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-
-// Импортируем картинку (цветок со стеблем или цветок-ноль)
-import fourZeroFourFlowers from '../assets/four-zero-four-flowers.png'
-// Если у тебя другой файл фоновых цветов, например errorFlowers.png, расскоментируй:
-// import errorFlowers from '../assets/errorFlowers.png'
-
+// Подключи твою ветку/цветок слева, если нужно, или фоновую картинку
+import notFoundFlower from '../assets/faq-flower.png' // Используй твой цветок из assets
 import '../App.css'
 
 const NotFound = () => {
   return (
     <div className="not-found-page">
-      {/* Декоративный крупный цветок слева / по центру */}
-      <img
-        src={fourZeroFourFlowers}
-        alt="Lover Flower 404"
-        className="not-found-bg-flower"
-      />
+      {/* Декоративный цветок слева */}
+      <img src={notFoundFlower} alt="" className="nf-flower" />
 
-      <div className="not-found-container">
-        {/* Композиция 404: 4 + Цветок-ноль + 4 */}
-        <div className="not-found-code">
-          <span className="digit">4</span>
-          <div className="flower-zero-placeholder">
-            {/* Если цветок не входит в единое изображение, он позиционируется здесь */}
-          </div>
-          <span className="digit">4</span>
+      {/* Огромный фоновый водяной знак */}
+      <div className="nf-bg-text">LOVER FLOWER</div>
+
+      <div className="nf-container">
+        {/* Гигантская цифра 404 с эффектом подсвечивания */}
+        <div className="nf-code-wrapper">
+          <span className="nf-code">404</span>
         </div>
 
-        {/* Текстовый блок и кнопка */}
-        <div className="not-found-content">
-          <span className="error-tag">ОШИБКА 404</span>
-          <h1 className="error-title">УПС... ТАКОЙ СТРАНИЦЫ НЕТ</h1>
-          
-          <Link to="/" className="not-found-btn">
+        <div className="nf-content">
+          <span className="nf-badge">ОШИБКА 404</span>
+          <h1 className="nf-title">УПС... ТАКОЙ СТРАНИЦЫ НЕТ</h1>
+          <p className="nf-description">
+            Возможно, она была перемещена или вовсе не существовала. <br />
+            Предлагаем вернуться в наш каталог и выбрать прекрасный букет!
+          </p>
+
+          <Link to="/" className="nf-button">
             НА ГЛАВНУЮ
           </Link>
         </div>
-
-        {/* Водяной знак */}
-        <div className="not-found-watermark">Lover Flower</div>
       </div>
     </div>
   )
